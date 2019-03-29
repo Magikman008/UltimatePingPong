31c87a2b300b73c5225d9060589189d6e8146428
Magikman008 <40332480+Magikman008@users.noreply.github.com>
2019-03-29 22:32:06 +0300

    Update Main.java

--- pingpong-master/src/application/Main.java
+++ pingpong-master/src/application/Main.java
@@ -16,65 +16,61 @@ public class Main extends Application {
 	
 	private static final int width = 800;
 	private static final int height = 600;
-	
-	private static final int RACKET_WIDTH = 10;
-	private static final int RACKET_HEIGHT = 90;
-	
+	private static final int racket_width = 10;
+	private static final int racket_height = 90;
 	private static final int rad = 30;
-	
 	double playerX = 0;
 	double playerY = height/2;
-	
-	double compX = width - RACKET_WIDTH;
+	double compX = width - racket_width;
 	double compY = height/2;
-	
 	double ballX = width/2 - rad/2;
 	double ballY = height/2 - rad/2;
-	
-	GraphicsContext gc;
-	
 	double ballYSpeed = 3;
 	double ballXSpeed = 3;
-	
 	boolean gameStarted;
-	
 	int comp = 0;
 	int player = 0;
 	String strcomp = String.valueOf(comp);
 	String strplay = String.valueOf(player);
 	double mouse;
+	GraphicsContext gc;
 	
 	private void drawTable() {
 		gc.setFill(Color.BLACK);
 		gc.fillRect(0, 0, width, height);	
 		gc.setFill(Color.WHITE);
-		gc.fillRect(width/2-1, 0, 2, height);		
+		gc.fillRect(width/2 - 1, 0, 2, height);		
 		gc.strokeText(strplay, width/2-10, 10);
 		gc.strokeText(strcomp, width/2+10, 10);
-		if(gameStarted) {
-			ballX+=ballXSpeed;
-			ballY+=ballYSpeed;
-			if ((playerY + RACKET_HEIGHT/2 > mouse ) && (playerY>0)) {
+		if (gameStarted) {
+			ballX += ballXSpeed;
+			ballY += ballYSpeed;
+			if ((playerY + racket_height/2 > mouse ) && (playerY>0)) {
 				playerY = playerY - 3;
 			}
-			if ((playerY + RACKET_HEIGHT/2 < mouse) && (playerY + RACKET_HEIGHT<height)) {
+			
+			if ((playerY + racket_height/2 < mouse) && (playerY + racket_height<height)) {
 				playerY = playerY + 3;
 			}
-			if ((compY + RACKET_HEIGHT/2 > ballY) && (ballXSpeed>0) && (ballX>width/2) && (compY>0)) {
+			
+			if ((compY + racket_height/2 > ballY) && (ballXSpeed>0) && (ballX>width/2) && (compY>0)) {
 				compY = compY - 3;
 			}
-			if ((compY + RACKET_HEIGHT/2 < ballY) && (ballXSpeed>0) && (ballX>width/2) && (compY + RACKET_HEIGHT<height)) {
+			
+			if ((compY + racket_height/2 < ballY) && (ballXSpeed>0) && (ballX>width/2) && (compY + racket_height<height)) {
 				compY = compY + 3;
 			}
+			
 			gc.fillOval(ballX, ballY, rad, rad);
 		} else {
 			gc.setStroke(Color.WHITE);
 			gc.setTextAlign(TextAlignment.CENTER);
 			gc.strokeText("Click to start", width/2, height/2);			
 		}
-		if(ballX<0) {
+		
+		if (ballX < 0) {
 			gameStarted = false;
-			compX = width - RACKET_WIDTH;
+			compX = width - racket_width;
 			compY = height/2;
 			ballX = width/2 - rad/2;
 			ballY = height/2 - rad/2;
@@ -82,9 +78,10 @@ public class Main extends Application {
 			strcomp = String.valueOf(comp);
 			ballXSpeed = ballXSpeed * (-1);
 		}
-		if(ballX>width) {
+		
+		if (ballX > width) {
 			gameStarted = false;
-			compX = width - RACKET_WIDTH;
+			compX = width - racket_width;
 			compY = height/2;
 			ballX = width/2 - rad/2;
 			ballY = height/2 - rad/2;
@@ -92,18 +89,20 @@ public class Main extends Application {
 			strplay = String.valueOf(player);
 			ballXSpeed=ballXSpeed * (-1);
 		}
-	
-		gc.fillRect(playerX, playerY, RACKET_WIDTH, RACKET_HEIGHT);
-		gc.fillRect(compX, compY, RACKET_WIDTH, RACKET_HEIGHT);
-		if (ballX + rad/2>  compX - RACKET_WIDTH) {
+		gc.fillRect(playerX, playerY, racket_width, racket_height);
+		gc.fillRect(compX, compY, racket_width, racket_height);
+		if (ballX + rad/2 >  compX - racket_width) {
 			ballXSpeed = ballXSpeed * (-1);
 		}
-		if ((ballX < playerX + RACKET_WIDTH) && (ballY + rad/2 > playerY) && (ballY + rad/2 < playerY + RACKET_HEIGHT)){
+		
+		if ((ballX < playerX + racket_width) && (ballY + rad/2 > playerY) && (ballY + rad/2 < playerY + racket_height)){
 			ballXSpeed = ballXSpeed * (-1);
 		}
+		
 		if (ballY > height - rad) {
 			ballYSpeed = ballYSpeed * (-1);
 		}
+		
 		if (ballY < 0) {
 			ballYSpeed = ballYSpeed * (-1);
 		}
